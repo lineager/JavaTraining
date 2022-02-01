2caecfe0e51731d3154b2c751a54fea902848dc3
Евгений Муравьев <emuravev@cinimex.ru>
2022-02-01 09:00:40 +0300

    start browser on remote server

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,13 +1,19 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -35,10 +41,20 @@ public class ApplicationManager {
         } catch (IOException e) {
             System.out.println("!!!!ACHTUNG file not found");
         }
-        if (browser.equals(properties.getProperty("web.browser"))) {
-            wd = new ChromeDriver();
-        }else {
-            wd = new FirefoxDriver();
+        if("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(properties.getProperty("web.browser"))) {
+                wd = new ChromeDriver();
+            } else {
+                wd = new FirefoxDriver();
+            }
+        } else {
+            try {
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                capabilities.setBrowserName(browser);
+                wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")),capabilities);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
         }
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
